31a5c0c22db5791f2421f07d5deefab697bd6435
Vinicius Souza <vinicius.templario@gmail.com>
2022-01-31 22:17:38 -0300

    Adicionando a classe EntidadeEmUsoException

---
+++ src/main/java/com/algaworks/algaschool/domain/exception/EntidadeEmUsoException.java
@@ -0,0 +1,13 @@
+package com.algaworks.algaschool.domain.exception;
+
+public class EntidadeEmUsoException extends NegocioException{
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public EntidadeEmUsoException(String mensagem) {
+		super(mensagem);
+		
+	}
+
+}
